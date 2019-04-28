c7040fb434e6422a0f6b8d9181d3ee4acb4eafd6
amanharitsh123 <amanharitsh123@gmail.com>
2019-04-28 23:42:55 +0530

    Support For C and Cpp added

--- compiler.py
+++ compiler.py
@@ -1,10 +1,10 @@
 import subprocess, os, time, signal
 
-def subprocess_cmd(command,inputfile,outputfile):
+def subprocess_cmd(command,inputfile,outputfile,time_limit,mem_limit):
 	process = subprocess.Popen(command,stdout=outputfile,stdin=inputfile,shell=True, preexec_fn=os.setsid)
-	is_timeout=wait_timeout(process,1)
+	is_timeout=wait_timeout(process,time_limit)
 	if is_timeout:
-		print("TLE!!")
+		print("Time Limit Exceeded!!")
 
 def wait_timeout(proc, seconds):
 	start = time.time()
@@ -17,24 +17,30 @@ def wait_timeout(proc, seconds):
 			return result
 		if time.time() >= end:
 			os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
-			print("killing")
 			return True
 		time.sleep(interval)
 
 
+dic_languages={1:"python3 test.py", 2:"gcc test.c;./a.out",3:"g++ test.cpp;./a.out"}
+command_to_compile = ""
+inputfile = open("inp.txt")
+sanitized_input = open("sanitized_input","w+")
+outputfile = open("output.txt","a+")
 
-command_to_compile="python3 test.py"
-inputfile=open("inp.txt")
-sanitized_input=open("sanitized_input","w+")
-outputfile=open("output.txt","a+")
-# Fetch Parameters
-parameters=inputfile.readline()
+# Fetch Parameters [language,time_limit,mem_limit]
+
+parameters = inputfile.readline()
+parameters = parameters.split()
+language = int(parameters[0])
+time_limit = int(parameters[1])
+mem_limit = int(parameters[2])
+
+command_to_compile = dic_languages[language]
+print("compiling ",command_to_compile)
 
 # Stripfile for the first line
 subprocess.Popen("sed '1d'  inp.txt",stdout=sanitized_input,stdin=inputfile,shell=True, preexec_fn=os.setsid)
-print("paramerters are ",parameters)
 
+# Changing input file to read mode and starting compilation
 sanitized_input=open("sanitized_input","r")
-subprocess_cmd(command_to_compile,sanitized_input,outputfile)
-
-# os.system("python3 test.py")
+subprocess_cmd(command_to_compile,sanitized_input,outputfile,time_limit,mem_limit)
