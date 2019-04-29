05e21c3651793159a9045cf7a07b73aaca42fa78
amanharitsh123 <amanharitsh123@gmail.com>
2019-04-29 21:22:06 +0530

    Absolute path added

--- docker_init.sh
+++ docker_init.sh
@@ -1,9 +1,19 @@
+set -x
 # remove any previous container
 sudo docker rm `sudo docker inspect --format="{{.Id}}" securo`
 # Build Docker Image
 
+#var="192.168.200.1"
+#echo ${var%.*}
+#
+#192.168.200
+
+path_of_file=`realpath "$0"`
+path_of_file=${path_of_file%/*}
+
+
 sudo docker build --tag=securo .
 # Running Docker container with output folder mounted
-sudo docker run --name=securo -d -v /home/amanharitsh/Desktop/minor2/securo/output:/output securo
+sudo docker run --name=securo -d -v $path_of_file/output:/output securo
 # Remove Docker Container
 # sudo docker rm `sudo docker inspect --format="{{.Id}}" securo` 
